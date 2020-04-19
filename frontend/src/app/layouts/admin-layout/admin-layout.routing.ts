21e887b40c077f2916b0d169092812636596e336
Ravindar Sharma <ravindar24sharma@gmail.com>
2020-04-19 19:12:47 +0530

    Adding frontend

---
+++ frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserComponent } from '../../pages/user/user.component';
+import { TableComponent } from '../../pages/table/table.component';
+import { TypographyComponent } from '../../pages/typography/typography.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { NotificationsComponent } from '../../pages/notifications/notifications.component';
+import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'sports', component: UserComponent },
+    { path: 'health', component: TableComponent },
+    { path: 'buisness', component: TypographyComponent },
+    { path: 'technology', component: IconsComponent },
+
+];
