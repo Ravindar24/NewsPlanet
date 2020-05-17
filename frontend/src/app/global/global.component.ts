691f5ee99c08243db8124bf69af683ca265ec8e3
Ravindar Sharma <ravindar24sharma@gmail.com>
2020-05-17 19:02:55 +0530

    local changes

--- frontend/src/app/global/global.component.ts
+++ frontend/src/app/global/global.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CovidService } from 'app/services/covid.service';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-global',
@@ -10,6 +11,11 @@ export class GlobalComponent implements OnInit {
   isFetchingCountrySummaryData = false;
   countrySummary;
   activeCases;
+  public canvas: any;
+  public ctx;
+  public globalCovidChart;
+
+
 
 
   constructor(private covidService: CovidService) {
@@ -28,6 +34,7 @@ export class GlobalComponent implements OnInit {
         this.activeCases = this.countrySummary.TotalConfirmed - (this.countrySummary.TotalDeaths + this.countrySummary.TotalRecovered);
         console.log(this.countrySummary.TotalConfirmed - (this.countrySummary.TotalDeaths + this.countrySummary.TotalRecovered));
         console.log(this.activeCases);
+        this.preparePieDashboard();
 
       },
       (err) => {
@@ -35,4 +42,74 @@ export class GlobalComponent implements OnInit {
       })
 
   }
+  preparePieDashboard() {
+    this.canvas = document.getElementById("globalCovidChart");
+    this.ctx = this.canvas.getContext("2d");
+    this.globalCovidChart = new Chart(this.ctx, {
+      type: 'pie',
+      data: {
+        labels: [1, 2, 3],
+        datasets: [{
+          label: "Emails",
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          backgroundColor: [
+            '#4acccd',
+            '#28c26d',
+            '#ef8157',
+            '#fcc468'
+          ],
+          borderWidth: 0,
+
+          data: [this.countrySummary.TotalConfirmed, this.countrySummary.TotalRecovered, this.countrySummary.TotalDeaths, this.activeCases]
+        }]
+      },
+
+      options: {
+
+        legend: {
+          display: false
+        },
+
+        pieceLabel: {
+          render: 'percentage',
+          fontColor: ['white'],
+          precision: 2
+        },
+
+        tooltips: {
+          enabled: false
+        },
+
+        scales: {
+          yAxes: [{
+
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              drawBorder: false,
+              zeroLineColor: "transparent",
+              color: 'rgba(255,255,255,0.05)'
+            }
+
+          }],
+
+          xAxes: [{
+            barPercentage: 1.6,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(255,255,255,0.1)',
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              display: false,
+            }
+          }]
+        },
+      }
+    });
+
+  }
+
 }
--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,50 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js';
 import { CovidService } from 'app/services/covid.service';
-declare var $ : any;
+declare var $: any;
 declare interface TableData {
   headerRow: string[];
   dataRows: string[][];
 }
 
 @Component({
-    selector: 'dashboard-cmp',
-    moduleId: module.id,
-    templateUrl: 'dashboard.component.html'
+  selector: 'dashboard-cmp',
+  moduleId: module.id,
+  templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
-  public canvas : any;
+  public canvas: any;
   public ctx;
   public chartColor;
   public chartEmail;
   public chartHours;
 
-  public cummulativeData : any;
-  public latestData : any;
-  public loader : boolean;
-  isFetchingStateData : boolean;
-  isFetchingCountryData : boolean;
-  tableHeaders : string[];
-  stateWiseData : any[];
+  public cummulativeData: any;
+  public latestData: any;
+  public loader: boolean;
+  isFetchingStateData: boolean;
+  isFetchingCountryData: boolean;
+  tableHeaders: string[];
+  stateWiseData: any[];
   activeCases;
 
   public tableData1: TableData;
-  
-  prepareTable(data : any[]) {
-    
-    
+
+  prepareTable(data: any[]) {
+
+
 
     let rows = [];
     data.forEach(element => {
-      rows.push(element.id,element.name,element.confirmed,element.active,element.recovered,element.deaths)
+      rows.push(element.id, element.name, element.confirmed, element.active, element.recovered, element.deaths)
     })
-    
+
   }
 
-  constructor(private covidService : CovidService) {
-    this.tableHeaders = ["S.No","State","Confirmed","Active","Recovered","Deaths"]
+  constructor(private covidService: CovidService) {
+    this.tableHeaders = ["S.No", "State", "Confirmed", "Active", "Recovered", "Deaths"]
     this.getStatewiseData();
     this.getCoronaHistory();
   }
@@ -54,29 +54,30 @@ export class DashboardComponent implements OnInit{
     this.covidService.getStatewiseCovidData().subscribe(
       (response) => {
         this.isFetchingStateData = false;
-        if(response){
+        if (response) {
           this.stateWiseData = response["data"]
         }
       },
-      (err)=> {
+      (err) => {
         this.isFetchingStateData = false;
       })
   }
 
   getCoronaHistory() {
-      this.isFetchingCountryData = true;
+    this.isFetchingCountryData = true;
     this.covidService.getCountryCovidData().subscribe(
       (response) => {
         this.isFetchingCountryData = false;
-        if(response){
+        if (response) {
           this.cummulativeData = response["data"];
           this.latestData = this.cummulativeData[this.cummulativeData.length - 1]
           this.activeCases = this.latestData.Confirmed - (this.latestData.Recovered + this.latestData.Deaths)
+          this.preparePieDashboard();
+        }
+      },
+      (err) => {
+        this.isFetchingCountryData = false;
       }
-    },
-    (err) => {
-      this.isFetchingCountryData = false;
-    }
     )
   }
 
@@ -85,198 +86,203 @@ export class DashboardComponent implements OnInit{
     $("#filterModal").modal('show');
   }
 
-    ngOnInit(){
+  preparePieDashboard() {
+    this.canvas = document.getElementById("chartEmail");
+    this.ctx = this.canvas.getContext("2d");
+    this.chartEmail = new Chart(this.ctx, {
+      type: 'pie',
+      data: {
+        labels: [1, 2, 3],
+        datasets: [{
+          label: "Emails",
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          backgroundColor: [
+            '#4acccd',
+            '#28c26d',
+            '#ef8157',
+            '#fcc468'
+          ],
+          borderWidth: 0,
 
+          data: [this.latestData.Confirmed, this.latestData.Recovered, this.latestData.Deaths, this.activeCases]
+        }]
+      },
 
-      this.chartColor = "#FFFFFF";
+      options: {
 
-      this.canvas = document.getElementById("chartHours");
-      this.ctx = this.canvas.getContext("2d");
-
-      this.chartHours = new Chart(this.ctx, {
-        type: 'line',
-
-        data: {
-          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
-          datasets: [{
-              borderColor: "#6bd098",
-              backgroundColor: "#6bd098",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
-            },
-            {
-              borderColor: "#f17e5d",
-              backgroundColor: "#f17e5d",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
-            },
-            {
-              borderColor: "#fcc468",
-              backgroundColor: "#fcc468",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
-            }
-          ]
+        legend: {
+          display: false
         },
-        options: {
-          legend: {
-            display: false
-          },
 
-          tooltips: {
-            enabled: false
-          },
+        pieceLabel: {
+          render: 'percentage',
+          fontColor: ['white'],
+          precision: 2
+        },
 
-          scales: {
-            yAxes: [{
+        tooltips: {
+          enabled: false
+        },
 
-              ticks: {
-                fontColor: "#9f9f9f",
-                beginAtZero: false,
-                maxTicksLimit: 5,
-                //padding: 20
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "#ccc",
-                color: 'rgba(255,255,255,0.05)'
-              }
+        scales: {
+          yAxes: [{
 
-            }],
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              drawBorder: false,
+              zeroLineColor: "transparent",
+              color: 'rgba(255,255,255,0.05)'
+            }
 
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent",
-                display: false,
-              },
-              ticks: {
-                padding: 20,
-                fontColor: "#9f9f9f"
-              }
-            }]
-          },
-        }
-      });
+          }],
 
-
-      this.canvas = document.getElementById("chartEmail");
-      this.ctx = this.canvas.getContext("2d");
-      this.chartEmail = new Chart(this.ctx, {
-        type: 'pie',
-        data: {
-          labels: [1, 2, 3],
-          datasets: [{
-            label: "Emails",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#e3e3e3',
-              '#4acccd',
-              '#fcc468',
-              '#ef8157'
-            ],
-            borderWidth: 0,
-            data: [342, 480, 530, 120]
+          xAxes: [{
+            barPercentage: 1.6,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(255,255,255,0.1)',
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              display: false,
+            }
           }]
         },
+      }
+    });
 
-        options: {
+  }
 
-          legend: {
-            display: false
-          },
-
-          pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
-          },
-
-          tooltips: {
-            enabled: false
-          },
-
-          scales: {
-            yAxes: [{
-
-              ticks: {
-                display: false
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "transparent",
-                color: 'rgba(255,255,255,0.05)'
-              }
-
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent"
-              },
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-        }
-      });
-
-      var speedCanvas = document.getElementById("speedChart");
-
-      var dataFirst = {
-        data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
-        fill: false,
-        borderColor: '#fbc658',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#fbc658',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8,
-      };
-
-      var dataSecond = {
-        data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
-        fill: false,
-        borderColor: '#51CACF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#51CACF',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
-
-      var speedData = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [dataFirst, dataSecond]
-      };
-
-      var chartOptions = {
-        legend: {
-          display: false,
-          position: 'top'
-        }
-      };
-
-      var lineChart = new Chart(speedCanvas, {
-        type: 'line',
-        hover: false,
-        data: speedData,
-        options: chartOptions
-      });
-    }
+  ngOnInit() {
 
 
+    //   this.chartColor = "#FFFFFF";
+
+    //   this.canvas = document.getElementById("chartHours");
+    //   this.ctx = this.canvas.getContext("2d");
+
+    //   this.chartHours = new Chart(this.ctx, {
+    //     type: 'line',
+
+    //     data: {
+    //       labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+    //       datasets: [{
+    //         borderColor: "#6bd098",
+    //         backgroundColor: "#6bd098",
+    //         pointRadius: 0,
+    //         pointHoverRadius: 0,
+    //         borderWidth: 3,
+    //         data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
+    //       },
+    //       {
+    //         borderColor: "#f17e5d",
+    //         backgroundColor: "#f17e5d",
+    //         pointRadius: 0,
+    //         pointHoverRadius: 0,
+    //         borderWidth: 3,
+    //         data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
+    //       },
+    //       {
+    //         borderColor: "#fcc468",
+    //         backgroundColor: "#fcc468",
+    //         pointRadius: 0,
+    //         pointHoverRadius: 0,
+    //         borderWidth: 3,
+    //         data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
+    //       }
+    //       ]
+    //     },
+    //     options: {
+    //       legend: {
+    //         display: false
+    //       },
+
+    //       tooltips: {
+    //         enabled: false
+    //       },
+
+    //       scales: {
+    //         yAxes: [{
+
+    //           ticks: {
+    //             fontColor: "#9f9f9f",
+    //             beginAtZero: false,
+    //             maxTicksLimit: 5,
+    //             //padding: 20
+    //           },
+    //           gridLines: {
+    //             drawBorder: false,
+    //             zeroLineColor: "#ccc",
+    //             color: 'rgba(255,255,255,0.05)'
+    //           }
+
+    //         }],
+
+    //         xAxes: [{
+    //           barPercentage: 1.6,
+    //           gridLines: {
+    //             drawBorder: false,
+    //             color: 'rgba(255,255,255,0.1)',
+    //             zeroLineColor: "transparent",
+    //             display: false,
+    //           },
+    //           ticks: {
+    //             padding: 20,
+    //             fontColor: "#9f9f9f"
+    //           }
+    //         }]
+    //       },
+    //     }
+    //   });
+
+
+
+    //   var speedCanvas = document.getElementById("speedChart");
+
+    //   var dataFirst = {
+    //     data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
+    //     fill: false,
+    //     borderColor: '#fbc658',
+    //     backgroundColor: 'transparent',
+    //     pointBorderColor: '#fbc658',
+    //     pointRadius: 4,
+    //     pointHoverRadius: 4,
+    //     pointBorderWidth: 8,
+    //   };
+
+    //   var dataSecond = {
+    //     data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
+    //     fill: false,
+    //     borderColor: '#51CACF',
+    //     backgroundColor: 'transparent',
+    //     pointBorderColor: '#51CACF',
+    //     pointRadius: 4,
+    //     pointHoverRadius: 4,
+    //     pointBorderWidth: 8
+    //   };
+
+    //   var speedData = {
+    //     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    //     datasets: [dataFirst, dataSecond]
+    //   };
+
+    //   var chartOptions = {
+    //     legend: {
+    //       display: false,
+    //       position: 'top'
+    //     }
+    //   };
+
+    //   var lineChart = new Chart(speedCanvas, {
+    //     type: 'line',
+    //     hover: false,
+    //     data: speedData,
+    //     options: chartOptions
+    //   });
+    // }
+
+  }
 }
