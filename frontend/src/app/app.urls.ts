6488df1a61dfd34f6fa1f2a6fcd04e973d27cd75
Ravindar Sharma <ravindar24sharma@gmail.com>
2020-05-25 20:57:15 +0530

    changing dashboard html and ts for new keys in json response namely(active and location)

--- frontend/src/app/app.urls.ts
+++ frontend/src/app/app.urls.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://127.0.0.1:5000"
 
-export const stateWiseCovidUrl = BASE_URL + "/get-corona-data/v1"
+export const stateWiseCovidUrl = BASE_URL + "/get-indian-states-data/v1"
 export const countryCovidUrl = BASE_URL + "/get-corona-history/v1"
 export const countrySummary = "https://api.covid19api.com/world/total"
