8c57679ea590e9a436d9399b42c6051f37e8c38f
shivam0071 <shivamkoundal01@gmail.com>
2020-05-25 20:37:45 +0530

    Country Wise Data for Table from Wiki

--- backend/corona/get_corona_data.py
+++ backend/corona/get_corona_data.py
@@ -1,7 +1,7 @@
 import requests
 import logging
 import pandas as pd
-from newsapp.endpoints import INDIA_STATE_WISE_URL,API_KEY_HEADERS,INDIA_COVID_HISTORY_URL, STATE_URL
+from newsapp.endpoints import INDIA_STATE_WISE_URL,API_KEY_HEADERS,INDIA_COVID_HISTORY_URL, STATE_URL, COUNTRIES_URL
 
 
 def get_data():
@@ -39,3 +39,15 @@ def get_state_wise_data_wiki():
     except Exception as ex:
         logging.Exception("Exception in State Wise Wiki API")
         return {"ERROR": "Problem with API"}
+
+def get_country_wise_data_wiki():
+    try:
+        world_table = pd.read_html(COUNTRIES_URL)[4][0:228]
+        world_table.drop([('Locations[e]', '229'), 'Ref.'], axis = 1, inplace=True)
+        world_table.columns = ["location", "cases", "deaths", "recovered"]
+        world_table_data =  world_table.to_dict(orient = 'records')
+        logging.info(f"Countries Example - {world_table_data[0]}")
+        return world_table_data
+    except Exception as ex:
+        logging.Exception("Exception in Country Wise Wiki API")
+        return {"ERROR": "Problem with Country Wise Wiki API"}
--- backend/newsapp/routes.py
+++ backend/newsapp/routes.py
@@ -1,5 +1,5 @@
 from flask import request
-from corona.get_corona_data import get_data,get_corona_history, get_state_wise_data_wiki
+from corona.get_corona_data import get_data,get_corona_history, get_state_wise_data_wiki, get_country_wise_data_wiki
 from newsapp import app
 
 @app.route("/get-corona-data/v1", methods=["GET"])
@@ -17,4 +17,8 @@ def state_wise_data_wiki():
     data = get_state_wise_data_wiki()
     return ({"data" : data})
 
+@app.route("/get-countries-data/v1", methods=["GET"])
+def country_wise_data_wiki():
+    data = get_country_wise_data_wiki()
+    return ({"data" : data})
     
