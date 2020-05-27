dedc9b0d292f8361eb2fdd3ef972b2668e02aa17
shivam0071 <shivamkoundal01@gmail.com>
2020-05-27 14:42:49 +0530

    caching table data for faster res and thread added

--- backend/corona/get_corona_data.py
+++ backend/corona/get_corona_data.py
@@ -1,8 +1,33 @@
 import requests
 import logging
+import sys
+from threading import Thread
+import time
 import pandas as pd
 from newsapp.endpoints import INDIA_STATE_WISE_URL,API_KEY_HEADERS,INDIA_COVID_HISTORY_URL, STATE_URL, COUNTRIES_URL
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+CACHE_STATE_DATA = []
+CACHE_COUNTRY_DATA = []
+RESET_CACHE = 0
+
+def clear_cache_thread():
+    global CACHE_STATE_DATA, CACHE_COUNTRY_DATA
+    reset_cache_timer = 0
+    logging.info("CACHING DATA INITIALLY") 
+    CACHE_STATE_DATA = get_state_wise_data_wiki()
+    CACHE_COUNTRY_DATA = get_country_wise_data_wiki()
+    while True:
+        if reset_cache_timer < 3600: # refresh every one hour, todo: add hardcoded data to config files
+            reset_cache_timer += 1
+            time.sleep(1)
+        elif reset_cache_timer == 3660:
+            logging.info("REFRESHING CACHE")
+            reset_cache_timer = 0
+            CACHE_STATE_DATA = []
+            CACHE_COUNTRY_DATA = []
+            CACHE_STATE_DATA = get_state_wise_data_wiki()
+            CACHE_COUNTRY_DATA = get_country_wise_data_wiki()
 
 def get_data():
     try:
@@ -28,12 +53,17 @@ def get_corona_history():
         return {"ERROR": "Problem with API"}
 
 def get_state_wise_data_wiki():
+    global CACHE_STATE_DATA
     try:
+        if CACHE_STATE_DATA:
+            logging.info("FETCHING FROM CACHED DATA")
+            return CACHE_STATE_DATA
         table = pd.read_html(STATE_URL)[5][3:40]
         table = table[[1,2,3,4,5]]
         table.columns = ["location", "cases", "deaths", "recovered", "active"]
         state_data = table.to_dict('records')
         logging.info(f" India Example - {state_data[0]}")
+        CACHE_STATE_DATA = state_data
         return state_data
         # {'location': 'Andaman and Nicobar Islands', 'cases': '33', 'deaths': '0', 'recovered': '33', 'active': '0'}
     except Exception as ex:
@@ -41,13 +71,23 @@ def get_state_wise_data_wiki():
         return {"ERROR": "Problem with API"}
 
 def get_country_wise_data_wiki():
+    global CACHE_COUNTRY_DATA
     try:
+        if CACHE_COUNTRY_DATA:
+            logging.info("FETCHING FROM CACHED DATA")
+            return CACHE_COUNTRY_DATA
         world_table = pd.read_html(COUNTRIES_URL)[4][0:228]
         world_table.drop([('Locations[e]', '229'), 'Ref.'], axis = 1, inplace=True)
         world_table.columns = ["location", "cases", "deaths", "recovered"]
         world_table_data =  world_table.to_dict(orient = 'records')
         logging.info(f"Countries Example - {world_table_data[0]}")
+        CACHE_COUNTRY_DATA = world_table_data
         return world_table_data
     except Exception as ex:
         logging.Exception("Exception in Country Wise Wiki API")
         return {"ERROR": "Problem with Country Wise Wiki API"}
+
+# to do, place this at apt place
+t1 = Thread(target = clear_cache_thread)
+logging.info("STARTING THREAD") 
+t1.start()
